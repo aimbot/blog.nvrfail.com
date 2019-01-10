6f4fbfd4913367859b04921f8d80114e7f3a6dcd
shawn <shawn@nvrfail.com>
2019-01-10 09:34:58 -0500

    post - seiko 5 analog drop

---
+++ src/pages/posts/2019-01-10-taking-a-step-back-from-tech/post.js
@@ -0,0 +1,6 @@
+export default {
+	title: `Screw smartwatches!`,
+	tags: ['analog'],
+	spoiler: 'Why notifications on your wrist is just something I am totally not looking forward to.',
+	getContent: () => import('./document.mdx'),
+}
