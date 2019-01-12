48d22b22a1d9e4edc3ab1017bf5c6e2238962a2e
shawn <shawn@nvrfail.com>
2019-01-11 21:59:35 -0500

    post - first video
    Fixing date

---
+++ src/pages/posts/2019-01-11-forcing-the-nerd-to-go-outside/post.js
@@ -0,0 +1,7 @@
+export default {
+	title: `I live my life one frame at a time!`,
+	tags: ['videography'],
+	spoiler:
+		'Vintage lenses, AF, focus peaking, bokeh, APS-C, Full Frame?? What? I thought tech had some cryptic terms.',
+	getContent: () => import('./document.mdx'),
+}
